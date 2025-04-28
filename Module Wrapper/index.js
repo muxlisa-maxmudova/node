7abefffe5d80081da246d914be356b3ea35a1b18
muxlisa-maxmudova <akobirurozov22@gmail.com>
2025-04-28 22:02:08 +0500

    this is exporting and importing variables

--- Module Wrapper/index.js
+++ Module Wrapper/index.js
@@ -1,6 +1,10 @@
-(function() {
-    console.log('Hello, Mukhlisa')
-})()// this is an anonymous function
+const {person, person1,person2} = require('./people')
+function say(username) {
+    console.log('Hello, ', username)
+}
+say(person)// this is an anonymous function
+say(person1)
+say(person2)
 
 
 
