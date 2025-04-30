54ad7d3c03953a6341df97d2774fed33aab86c16
muxlisa-maxmudova <akobirurozov22@gmail.com>
2025-04-30 14:26:58 +0500

    copied and sent the copy to the given dest

--- FS_Modules/promise/index.js
+++ FS_Modules/promise/index.js
@@ -1,9 +1,8 @@
 import * as fs from "fs/promises";
 try {
-   const data = await fs.readFile("c:\\nodejs\\hello.txt" );
-    console.log(data.toString()); // if we do not add
-    // toString() it is going to render in buffer, or we can just add 'utf8' after indicating path
-}
-catch (e) {
-    console.error(e);
-}
+     await fs.copyFile('c:\\nodejs\\hello.txt', 'c:\\nodejs/home\\copiedHello.txt')
+     console.log("File copied")
+ }
+ catch (error) {
+     console.log(error)
+ }
