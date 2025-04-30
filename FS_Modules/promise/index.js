d6ae5e3970fa645ba8a8a2b81ddb55437dd33fe4
muxlisa-maxmudova <akobirurozov22@gmail.com>
2025-04-30 14:12:57 +0500

    created a file notes inside nodejs with a text hello world

--- FS_Modules/promise/index.js
+++ FS_Modules/promise/index.js
@@ -1,10 +1,8 @@
 import * as fs from "fs/promises";
-    try{
-        const files = await fs.readdir('c:\\nodejs')
-          for (const file of files){
-              console.log(file)
-          }
-    }
-    catch (error){
-        console.log(error)
-    }
+try {
+    await fs.writeFile("c:\\nodejs\\hello.txt", "Hello World!");
+    console.log("File created");
+}
+catch (e) {
+    console.error(e);
+}
