ed1062064572d9ce3e30efee79414da1b60fd231
muxlisa-maxmudova <akobirurozov22@gmail.com>
2025-05-05 06:04:36 +0500

    to see statistics about a file or a directory

--- FS_Modules/promise/index.js
+++ FS_Modules/promise/index.js
@@ -1,9 +1,17 @@
 import * as fs from "fs/promises";
-import * as constants from "node:constants";
+//import * as constants from "node:constants";
+// try {
+//      await fs.copyFile('c:\\nodejs\\hello.txt', 'c:\\nodejs/home\\copiedHello.txt', constants.COPYFILE_EXCL)
+//      console.log("File copied")
+//  }
+//  catch (error) {
+//      console.log(error)
+//  }
+
 try {
-     await fs.copyFile('c:\\nodejs\\hello.txt', 'c:\\nodejs/home\\copiedHello.txt', constants.COPYFILE_EXCL)
-     console.log("File copied")
- }
- catch (error) {
-     console.log(error)
- }
+    const stats = await fs.stat('c:\\nodejs\\hello.txt')
+    console.log(stats)
+}
+catch (e) {
+    console.error(e)
+}
