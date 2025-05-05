60a76746d88470ef47433c7672161a7443d9fce6
muxlisa-maxmudova <akobirurozov22@gmail.com>
2025-05-05 05:55:19 +0500

    another param

--- FS_Modules/promise/index.js
+++ FS_Modules/promise/index.js
@@ -1,6 +1,7 @@
 import * as fs from "fs/promises";
+import * as constants from "node:constants";
 try {
-     await fs.copyFile('c:\\nodejs\\hello.txt', 'c:\\nodejs/home\\copiedHello.txt')
+     await fs.copyFile('c:\\nodejs\\hello.txt', 'c:\\nodejs/home\\copiedHello.txt', constants.COPYFILE_EXCL)
      console.log("File copied")
  }
  catch (error) {
